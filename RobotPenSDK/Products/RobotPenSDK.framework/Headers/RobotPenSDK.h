eb576df7fc6ead2c219a231b6f4e75a14cd7d60f
RobotPen <lusz@robotpen.cn>
2016-03-26 16:09:52 +0800

    build framework

--- RobotPenSDK/Products/RobotPenSDK.framework/Headers/RobotPenSDK.h
+++ RobotPenSDK/Products/RobotPenSDK.framework/Headers/RobotPenSDK.h
@@ -17,4 +17,4 @@ FOUNDATION_EXPORT const unsigned char SmartPenCoreVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <SmartPenCore/PublicHeader.h>
 
 
-#import <SmartPenCore/SmartPenService.h>
+#import <RobotPenSDK/RobotPenService.h>
